efa526f2780e729b1212d894b2006db42846eaed
Rishit Pandey <rishpandey8097@gmail.com>
2021-03-27 16:58:29 +0530

    navbar icons

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import IconButton from '@material-ui/core/IconButton';
 
 interface NavBarProps {}
 
@@ -23,11 +25,19 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 const NavBar: React.FC<NavBarProps> = ({}) => {
-  const classes = useStyles();
+  const classes: any = useStyles();
   return (
     <>
       <AppBar position='static' className={classes.appbar}>
         <Toolbar>
+          <IconButton
+            edge='start'
+            className={classes.menuButton}
+            color='inherit'
+            aria-label='menu'
+          >
+            <ListAltIcon />
+          </IconButton>
           <Typography variant='h6' className={classes.title}>
             Your Personal Task Planner
           </Typography>
